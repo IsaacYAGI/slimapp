e4a1307c4b03eb17c2dc4ede2016670064f45f1c
Isaac Yriarte <isaacyriarte@gmail.com>
2017-10-16 16:26:11 -0400

    Se agregan las rutas de obtener todos y un solo customer

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -4,9 +4,57 @@
 
   $app = new \Slim\App;
 
+  //Ruta para obtener todos los customers 
   $app->get('/api/customers',function(Request $request, Response $response){
 
-    echo "CUSTOMERS";
+    $sql = "select * from customers;";
+
+    try {
+      //Get DB object
+
+      $db = new db();
+
+      $db = $db->connect();
+
+      $stmt = $db->query($sql);
+
+      $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+      $db = null;
+
+      echo json_encode($customers); 
+      
+    } catch (PDOException $e) {
+      echo '{"error":{"text":'.$e->getMessage().'}}';
+    }
+
+  });
+
+  //Ruta para obtener un customer
+  $app->get('/api/customer/{id}',function(Request $request, Response $response){
+
+    $id = $request->getAttribute('id');
+
+    $sql = "select * from customers where id = $id;";
+
+    try {
+      //Get DB object
+
+      $db = new db();
+
+      $db = $db->connect();
+
+      $stmt = $db->query($sql);
+
+      $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+      $db = null;
+
+      echo json_encode($customer); 
+      
+    } catch (PDOException $e) {
+      echo '{"error":{"text":'.$e->getMessage().'}}';
+    }
 
   });
 
