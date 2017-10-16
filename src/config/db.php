1c030be46a845b19a35fa4e70993e8393ad79cdd
Isaac Yriarte <isaacyriarte@gmail.com>
2017-10-16 15:50:38 -0400

    Se agrega la clase de conexion del BDD

---
+++ src/config/db.php
@@ -0,0 +1,36 @@
+<?php
+
+  /**
+  * 
+  */
+  class db{
+
+    private $dbhost = "localhost";
+    private $dbuser = "root";
+    private $dbpass = "";
+    private $dbname = "slimapp";
+
+    public function getDbHost(){
+      return $this->$dbhost;
+    }
+    
+    public function getDbUser(){
+      return $this->$dbuser;
+    }
+    public function getDbPass(){
+      return $this->$dbpass;
+    }
+    public function getDbName(){
+      return $this->$dbname;
+    }
+
+
+    public function connect(){
+      $mysql_connect_str = "mysql:host=$this->getDbHost();dbname=$this->getDbName()";
+      $dbConnection = new PDO($mysql_connect_str, $this->getDbUser(), $this->getDbPass());
+      $dbConnection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+      return  $dbConnection;
+    }
+  }
+
